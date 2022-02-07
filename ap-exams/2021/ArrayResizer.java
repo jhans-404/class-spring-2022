57ecb2a0e11496109c66d63128a324a0c5f842c2
Joshua Hans <jhans@schools.nyc.gov>
2022-02-07 14:27:37 -0500

    2021 Q4

---
+++ ap-exams/2021/ArrayResizer.java
@@ -0,0 +1,12 @@
+public class ArrayResizer {
+  public static void main(String[] args) {
+    int[][] testArray = {{2, 1, 0},
+                         {1, 3, 2},
+                         {0, 0, 0},
+                         {4, 5, 6},
+                        }
+    
+  } // end main method
+
+
+} // end class
