f1ceb60677c50667c64a4812ed40505101aa60a5
Joshua Hans <jhans@schools.nyc.gov>
2022-02-07 14:28:17 -0500

    required methods added

--- ap-exams/2021/ArrayResizer.java
+++ ap-exams/2021/ArrayResizer.java
@@ -5,8 +5,20 @@ public class ArrayResizer {
                          {0, 0, 0},
                          {4, 5, 6},
                         }
-    
+
+    System.out.println(isNonZeroRow(testArray));
   } // end main method
 
+  public static boolean isNonZeroRow(int[][] arr, int r) {
+
+  } // end isNonZeroRow method
+
+  public static int[][] resize(int[][] arr) {
+
+  } // end resize method
+
+  public static int numNonZeroRows(int[][] arr) {
+
+  } // end numNonZeroRows method
 
 } // end class
