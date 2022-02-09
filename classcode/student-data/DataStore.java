694a9ed49083971d26c2b7d1bd418479e0b209b2
Joshua Hans <jhans@schools.nyc.gov>
2022-02-09 11:47:01 -0500

    starter code

---
+++ classcode/student-data/DataStore.java
@@ -0,0 +1,34 @@
+public class DataStore {
+  public static void main(String[] args) {
+    // student 1
+    String id1 = "1234567";
+    String name1 = "Jimmy Smith";
+    boolean cc1 = true;
+    int[] grades1 = {90, 80, 95, 90, 100};
+
+    // student 2
+    String id2 = "9876543";
+    String name2 = "Maria Hernandez";
+    boolean cc2 = false;
+    int[] grades2 = {95, 100, 100, 90, 90};
+
+    // student 3
+    String id3 = "1357911";
+    String name3 = "Jamal Jenkins";
+    boolean cc3 = false;
+    int[] grades3 = {85, 85, 95, 100, 90};
+
+    // student 4
+    String id4 = "2468101";
+    String name4 = "Ramiz Ahmed";
+    boolean cc4 = true;
+    int[] grades4 = {90, 100, 75, 85, 90};
+
+    // student 5
+    String id5 = "1928374";
+    String name5 = "Michael Green";
+    boolean cc5 = true;
+    int[] grades5 = {80, 80, 65, 55, 90};
+  } // end main method
+
+} // end class
---
+++ classcode/student-data/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+  // fields, instance varaibles (data)
+  // each instance of the Student class gets a copy of each variable
+  private String id;
+  private String name;
+  private boolean closeContact;
+  private int[] grades;
+
+
+
+}
